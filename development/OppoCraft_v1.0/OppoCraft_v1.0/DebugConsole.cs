e8d2a9a5af7064c41e0e1a85a323977574db1cd6
Freens77 <freens1977@gmail.com>
2013-10-27 11:39:47 -0400

    Added a debugger class to display messages on the screen for debugging purposes in development.
    --Debugger class has is scrollable, so if the messages go off the screen you can scroll with the mouse to see them all

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/DebugConsole.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OppoCraft
+{
+    public class DebugConsole
+    {
+        Game1 theGame;
+
+        LinkedList<String> msgList;
+        private int rowSize;
+
+        public int scrollRow;
+
+        public DebugConsole(Game1 g)
+        {
+            this.theGame = g;
+            this.msgList = new LinkedList<string>();
+            this.rowSize = 12;
+            this.scrollRow = 0; 
+        }
+
+        public void AddMessage(string msg)
+        {
+            this.msgList.AddLast(msg);
+        }
+
+        public void RenderMessages()
+        {
+            int currMsg = 0;
+
+            foreach (string item in msgList)
+            {
+                this.theGame.renderSystem.DrawText(item, new Vector2(5, this.rowSize * currMsg - this.scrollRow));
+                currMsg++;
+            }
+        }
+    }
+}
