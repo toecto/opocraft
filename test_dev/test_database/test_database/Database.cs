78fc519c00284dc6903c53d2a4144fe5c9d0ec29
Bhargavigurram <bhargavigurram5@gmail.com>
2013-10-29 11:15:56 -0400

    Database testing application created

---
+++ test_dev/test_database/test_database/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test_database
+{
+    class Database
+    {
+        ///props
+        ///
+        //constructor
+
+        //Connect(CONstr)
+
+        //Query(sqlString)
+
+        //NonQuery(sqlSqring)
+
+    }
+}
+
+
