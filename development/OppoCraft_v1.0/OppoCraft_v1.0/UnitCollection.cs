a67f736844ea8fe740c55b4d2d656c76b1948c02
tracyljeff <tracyljeff@gmail.com>
2013-11-14 23:43:39 -0500

    create Unit and UnitCollection class. Update to github.

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/UnitCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OppoCraft
+{
+    public class UnitCollection : Dictionary<int, Unit>
+    {
+        public Unit getById(int id)
+        {
+            if (this.ContainsKey(id))
+                return this[id];
+            return null;
+        }
+
+        new public void Remove(int id)
+        {
+            Unit u = this.getById(id);
+            if (u != null)
+                this.Remove(u);
+        }
+
+        public void Remove(Unit u)
+        {
+            this.Remove(u.id);
+        }
+
+        public virtual void Add(Unit u)
+        {
+            this.Add(u.id, u);
+        }
+
+    }
+    
+}
