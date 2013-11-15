b4544e96bc97b08c92ada6ea2e7231998c36991c
holmesli <holmeslixu@gmail.com>
2013-11-14 22:56:55 -0500

    I have pushed the CamandMovement class including onstart abstract method and MoveHandler method--Xu

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/ComandMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+
+namespace OppoCraft
+{
+    public class ComandMovement: Task
+    {
+        Vector2 delta;
+        Vector2 location;
+        Vector2 destination;
+        public ComandMovement(WorldCoords dest)
+        {
+            this.destination = dest.getVector2();
+        }
+        public override void onStart()
+        {
+            this.location = this.unit.location.getVector2();
+            float distance = Vector2.Distance(this.location, this.destination);
+            this.delta = Vector2.Divide(Vector2.Subtract(this.destination, this.location), distance);
+        }
+    }
+}
