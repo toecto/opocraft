53baadfde9f0bb774d9c9d025a05c955b153dfba
assashin23 <mitchaelsepe@gmail.com>
2013-11-15 11:06:21 -0500

    Editted Task.cs.  Added Tick, onStart, and onFinish methods

--- development/OppoCraft_v1.0/OppoCraft_v1.0/Task.cs
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Task.cs
@@ -7,5 +7,18 @@ namespace OppoCraft_v1._0
 {
     class Task
     {
+        public Unit unit;
+
+        public virtual bool Tick() 
+        {
+            return false;
+        }
+        public virtual void onStart() 
+        {
+        }
+
+        public virtual void onFinish() 
+        {
+        }
     }
 }
