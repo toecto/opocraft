a42fda5174320a8ea1c9ded70ee981a855d0a5df
holmesli <holmeslixu@gmail.com>
2013-11-26 14:24:03 -0500

    I fixed Tick method

--- development/OppoCraft_v1.0/OppoCraft_v1.0/PatrolArea.cs
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/PatrolArea.cs
@@ -8,21 +8,45 @@ namespace OppoCraft
 {
     public class PatrolArea: Task
     {
+
+        int currStep;
+        int totalSteps;
+        WorldPath worldPath;
+        Vector2 destination;
+        WorldCoords area;
         
-        WorldPath currPath;
-        WorldCoords w;
-        
-       public TaskGoTo patrol;
         public PatrolArea(WorldPath p)
+        {   
+            this.worldPath = p;
+        }
+        public void GetPath()
         {
-            this.currPath = p;
-           
+            this.worldPath = this.unit.theGame.theGrid.thePathFinder.GetPath(this.unit.location, this.area);
+            if (this.worldPath == null)
+                return;
+            this.totalSteps = this.worldPath.Count();
+
+            this.destination = this.worldPath.First.Value.getVector2();
         }
 
         public override bool Tick()
         {
+            if (this.worldPath != null)
+            {
+                for (int i = 0; i <= this.worldPath.Count(); i++)
+                {
+                    int number = this.worldPath.Count();
+                    this.destination = this.worldPath.Count(i) - 1;
+                }
+
+                
+                return true;
+            }
             
-            return base.Tick();
+            else
+            {
+                return  false;
+            }
         }
 
        
