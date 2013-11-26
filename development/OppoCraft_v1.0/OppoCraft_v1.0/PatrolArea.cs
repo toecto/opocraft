9655f32a5568049c0ed68e8e538b61c095226bf2
holmesli <holmeslixu@gmail.com>
2013-11-25 22:25:07 -0500

    Create patrolArea  class

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/PatrolArea.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OppoCraft
+{
+    public class PatrolArea: Task
+    {
+        
+        WorldPath currPath;
+        WorldCoords w;
+        
+       public TaskGoTo patrol;
+        public PatrolArea(WorldPath p)
+        {
+            this.currPath = p;
+           
+        }
+
+        public override bool Tick()
+        {
+            
+            return base.Tick();
+        }
+
+       
+
+        public override void onStart()
+        {
+           
+        }
+
+        public override void onFinish()
+        {
+            base.onFinish();
+        }
+    }
+}
