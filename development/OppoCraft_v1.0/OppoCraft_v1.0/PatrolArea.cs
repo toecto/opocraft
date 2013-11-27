f5d9d31c7d90e1bc47f234c94fc69c7e0355c689
holmesli <holmeslixu@gmail.com>
2013-11-26 22:45:05 -0500

    I fixed the constructor and tick method

--- development/OppoCraft_v1.0/OppoCraft_v1.0/PatrolArea.cs
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/PatrolArea.cs
@@ -8,45 +8,32 @@ namespace OppoCraft
 {
     public class PatrolArea: Task
     {
-
-        int currStep;
-        int totalSteps;
-        WorldPath worldPath;
-        Vector2 destination;
-        WorldCoords area;
+        WorldCoords currWorldPath;
+        WorldCoords destination;
         
         public PatrolArea(WorldPath p)
-        {   
-            this.worldPath = p;
-        }
-        public void GetPath()
         {
-            this.worldPath = this.unit.theGame.theGrid.thePathFinder.GetPath(this.unit.location, this.area);
-            if (this.worldPath == null)
-                return;
-            this.totalSteps = this.worldPath.Count();
-
-            this.destination = this.worldPath.First.Value.getVector2();
+           
+            Random random = new Random();
+            int worldIndex = random.Next(p.Count());
+            this.destination = p[worldIndex];
+           
+            
         }
+        
 
         public override bool Tick()
         {
-            if (this.worldPath != null)
+            if (this.currWorldPath == this.destination)
             {
-                for (int i = 0; i <= this.worldPath.Count(); i++)
-                {
-                    int number = this.worldPath.Count();
-                    this.destination = this.worldPath.Count(i) - 1;
-                }
-
-                
-                return true;
+                return false;
             }
             
             else
             {
-                return  false;
+                this.unit.task.Add(new TaskGoTo(this.destination));
             }
+            return true;
         }
 
        
