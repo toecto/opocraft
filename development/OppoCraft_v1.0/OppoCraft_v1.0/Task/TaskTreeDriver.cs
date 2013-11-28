9054d4236f71c79169f8459618b444eae50da338
assashin23 <mitchaelsepe@gmail.com>
2013-11-27 21:07:49 -0500

    Added Grass.png and Grass.psd to Git
    Updated Decorations.psd as well

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskTreeDriver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OppoCraft
+{
+    class TaskTreeDriver : Task
+    {
+        /*
+         * if hp is 0, change tree state to dead
+         * if tree is still growing, lumberjack unit cannot hit it
+         * 
+         * 
+         * 
+         */ 
+        public override bool Tick()
+        {
+            if (this.unit.currHP == 0) 
+            {
+                testClient.OppoMessage msg = new testClient.OppoMessage(testClient.OppoMessageType.ChangeState);
+                msg["state"] = (int)Unit.State.Dying;
+                this.unit.AddCommand(msg);
+            }
+            if (this.unit.state)
+            {
+                testClient.OppoMessage msg = new testClient.OppoMessage(testClient.OppoMessageType.ChangeState);
+                msg["state"] = (int)Unit.State.Dying;
+                this.unit.AddCommand(msg);
+            }
+            
+        }
+
+    }
+}
