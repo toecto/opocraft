afdbc7f9742c64e52dcebd59c7caec4d3d939020
assashin23 <mitchaelsepe@gmail.com>
2013-12-03 10:57:56 -0500

    Updates to Task.cs, TaskCollection.cs, TaskManager.cs, TaskTreeDriver.cs

--- development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskManager.cs
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskManager.cs
@@ -9,60 +9,135 @@ namespace OppoCraft
     {
         Unit unit;
         TaskCollection tasks;
+        Dictionary<string, Object> sharedMsg;
+        LinkedList<KeyValuePair<bool, Object>> tasksChanges;
 
         public TaskManager(Unit u)
         {
             this.unit = u;
+            this.sharedMsg = new Dictionary<string, object>();
             this.tasks = new TaskCollection();
+            this.tasksChanges = new LinkedList<KeyValuePair<bool, Object>>();
         }
 
         public void Tick()
         {
-            TaskCollection toRemove = new TaskCollection();
-            foreach(Task t in this.tasks)
+            foreach (KeyValuePair<Type, Task> item in this.tasks)
             {
-                if (!t.Tick())
-                    toRemove.AddLast(t);
+                if (!item.Value.Tick())
+                    this.Remove(item.Key);
             }
-            foreach (Task t in toRemove)
-            {
-                 this.Remove(t);
-            }
-            toRemove.Clear();  
+            this.applyChanges();
+
+        }
+
+        public TaskCollection getTasks()
+        {
+            return this.tasks;
         }
 
         public void Add(Task t)
         {
-            t.unit = this.unit;
-            this.tasks.AddLast(t);
-            t.onStart();
+            this.tasksChanges.AddLast(new KeyValuePair<bool, Object>(true, t));
         }
 
-        public void AddUnique(Task task)
+        public void Remove(Type t)
         {
-            this.RemoveByType(task.GetType());
-            this.Add(task);
+            this.tasksChanges.AddLast(new KeyValuePair<bool, Object>(false, t));
         }
 
-        public void RemoveByType(System.Type TypeToRemove)
+        internal void Clear()
         {
-            TaskCollection toRemove = new TaskCollection();
-            foreach (Task t in this.tasks)
+            this.tasksChanges.AddLast(new KeyValuePair<bool, Object>(false, null));
+        }
+
+
+
+        public bool isRunning(Type t)
+        {
+            return this.tasks.ContainsKey(t);
+        }
+
+        public bool checkShared(string name)
+        {
+            return this.sharedMsg.ContainsKey(name);
+        }
+
+        public T removeShared<T>(string name)
+        {
+            if (!this.checkShared(name)) return default(T);
+            Object rez = this.sharedMsg[name];
+            this.sharedMsg.Remove(name);
+            return (T)rez;
+        }
+
+        public T getShared<T>(string name)
+        {
+            if (!this.checkShared(name)) return default(T);
+            return (T)this.sharedMsg[name];
+        }
+
+        public void setShared(string name, Object data)
+        {
+            this.sharedMsg.Remove(name);
+            this.sharedMsg.Add(name, data);
+        }
+
+
+        //private---------------------------------------------
+        public void applyChanges()
+        {
+            LinkedListNode<KeyValuePair<bool, Object>> cursor = this.tasksChanges.First;
+            KeyValuePair<bool, Object> item;
+            while(cursor!=null)
             {
-                if (t.GetType() == TypeToRemove)
-                    toRemove.AddLast(t);
+                item = cursor.Value;
+                if (item.Key == true)
+                {
+                    this._actualAdd((Task)item.Value);
+                }
+                else
+                {
+                    if (item.Value == null)
+                        this._actualClear();
+                    else
+                        this._actualRemove((Type)item.Value);
+                }
+                cursor=cursor.Next;
             }
-            foreach (Task t in toRemove)
-            {
-                this.Remove(t);
-            }
-            toRemove.Clear();
+
+            this.tasksChanges.Clear();
         }
 
-        public void Remove(Task t)
+        private void _actualClear()
         {
-            t.onFinish();
+            LinkedList<Type> toRemove = new LinkedList<Type>();
+            foreach (KeyValuePair<Type, Task> item in this.tasks)
+            {
+                toRemove.AddLast(item.Key);
+            }
+            foreach (Type item in toRemove)
+            {
+                this._actualRemove(item);
+            }
+            this.tasks.Clear();
+        }
+
+        private void _actualAdd(Task task)
+        {
+            task.unit = this.unit;
+            this.tasks.Remove(task.GetType());
+            this.tasks.Add(task.GetType(), task);
+            task.onStart();
+        }
+
+        private void _actualRemove(System.Type t)
+        {
+            if (this.tasks.ContainsKey(t))
+                this.tasks[t].onFinish();
             this.tasks.Remove(t);
         }
+
+
     }
 }
--- development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskTreeDriver.cs
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskTreeDriver.cs
@@ -2,34 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using testClient;
 
 namespace OppoCraft
 {
-    class TaskTreeDriver : Task
+    class TaskTreeDriver: Task
     {
-        /*
-         * if hp is 0, change tree state to dead
-         * if tree is still growing, lumberjack unit cannot hit it
-         * 
-         * 
-         * 
-         */ 
-        public override bool Tick()
+        enum Status
         {
-            if (this.unit.currHP == 0) 
-            {
-                testClient.OppoMessage msg = new testClient.OppoMessage(testClient.OppoMessageType.ChangeState);
-                msg["state"] = (int)Unit.State.Dying;
-                this.unit.AddCommand(msg);
-            }
-            if (this.unit.state)
-            {
-                testClient.OppoMessage msg = new testClient.OppoMessage(testClient.OppoMessageType.ChangeState);
-                msg["state"] = (int)Unit.State.Dying;
-                this.unit.AddCommand(msg);
-            }
-            
+            Adult,
+            Died,
+            Collected,
+            Grow,
         }
 
+        Status status = Status.Adult;
+
+
+        public override void onStart()
+        {
+            this.unit.status = "Adult";
+        }
+
+        public override bool Tick()
+        {
+            if (!this.unit.alive && this.status == Status.Adult)
+            {
+                OppoMessage msg = new OppoMessage(OppoMessageType.ChangeState);
+                msg.Text["startact"] = "Die";
+                msg.Text["status"] = "ReadyToCollect";
+                this.unit.AddCommand(msg);
+                this.status = Status.Died;
+            }
+
+            if (this.status == Status.Died && this.unit.status == "ReadyToCollect")
+            {
+                OppoMessage msg = new OppoMessage(OppoMessageType.ChangeState);
+                msg.Text["status"] = "Collected";
+                msg.Text["startact"] = "Collected";
+                this.unit.AddCommand(msg);
+            }
+
+            if (this.status == Status.Died && this.unit.status == "Collected")
+            {
+                OppoMessage msg = new OppoMessage(OppoMessageType.TreeGrow);
+                msg["cooldown"] = 400;
+                this.unit.AddCommand(msg);
+                this.status = Status.Grow;
+            }
+
+            if (this.status == Status.Grow && this.unit.status == "Adult")
+            {
+                this.status = Status.Adult;
+            }
+
+
+            return true;
+        }
+
+        public override void onFinish()
+        {
+
+        }
     }
 }
