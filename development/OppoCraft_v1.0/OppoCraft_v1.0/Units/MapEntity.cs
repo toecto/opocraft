d6e2a9c40e347f1469e178320902f619c8374bd2
tracyljeff <tracyljeff@gmail.com>
2013-12-06 10:35:27 -0500

    Created as Abstract, as this will contain the common attributes and functions for inherited classes, such as a link to the Game, Size, Location, settings; all methods are virtual and will be implemented via the sub-class.

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Units/MapEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using testClient;
+
+namespace OppoCraft
+{
+    public abstract class MapEntity
+    {
+        public Game1 theGame;
+        public WorldCoords size = new WorldCoords(40, 40);
+        public WorldCoords location = new WorldCoords(1, 1);
+        public OppoMessage settings;
+
+        public int uid = 0;
+        public int cid = 0;
+
+        public GridCoords locationGrid { get { return this.theGame.theGrid.getGridCoords(this.location); } }
+
+        public virtual void onStart()
+        {
+        }
+
+
+        public virtual void onFinish()
+        {
+        }
+
+
+        public virtual void Tick()
+        {
+        }
+
+        public virtual void Render(RenderSystem render)
+        {
+        }
+    }
+}
