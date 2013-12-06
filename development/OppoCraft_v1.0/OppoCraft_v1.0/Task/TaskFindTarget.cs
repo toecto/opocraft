90a2709a813dc6c17b5605d3bff8cc371b5a8019
holmesli <holmeslixu@gmail.com>
2013-12-05 23:32:57 -0500

    Inherited from Task: Tick() method logic handles checking for a specified target unit within a minimum/maximum distance and if conditions are met it will set the targetUnit for a Unit will be set to the "found" unit

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskFindTarget.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OppoCraft 
+{
+    class TaskFindTarget : Task
+    {
+        string type;
+        bool anySide;
+
+        public TaskFindTarget(string type, bool anySide=false)
+        {
+            this.type = type;
+            this.anySide = anySide;    
+        }
+
+        public override bool Tick()
+        {
+            double minDistance = 0, checkDistance;
+            Unit target=null, unit;
+            foreach (KeyValuePair<int, MapEntity> item in this.unit.theGame.map)
+            {
+                if (item.Value.GetType() != typeof(Unit)) continue;
+                unit = (Unit)item.Value;
+                if (unit.cid == this.unit.cid && !anySide) continue;
+                if (unit.uid == this.unit.uid) continue;
+                if (unit.type != this.type) continue;
+
+                if (!unit.alive) continue;
+
+                checkDistance=this.unit.location.Distance(item.Value.location);
+                if (checkDistance < minDistance || minDistance == 0)
+                {
+                    minDistance = checkDistance;
+                    target = unit;
+                }
+                    
+            }
+
+            if (target != null)
+            {
+                this.unit.task.setShared("targetUnit", target);
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
