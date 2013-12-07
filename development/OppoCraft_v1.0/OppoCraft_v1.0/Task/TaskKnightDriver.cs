2cb16a55f6ca8251e428409bc2814adb3d116722
Bhargavigurram <bhargavigurram5@gmail.com>
2013-12-06 21:09:23 -0500

    Inherits from task.Task added to task collection for a unit; Adds Mortality Task to the task collection for the unit; Tick() method determines if specific tasks are running and handles it accordingly.

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskKnightDriver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OppoCraft
+{
+    class TaskKnightDriver : Task
+    {
+
+        public override bool Tick()
+        {
+            if (!this.unit.task.isRunning(typeof(TaskFindTarget)) && !this.unit.task.isRunning(typeof(TaskFight)))
+            {
+                this.unit.task.Add(new TaskFindTarget("Knight"));
+            }
+
+            if (!this.unit.task.isRunning(typeof(TaskFight)) && this.unit.task.checkShared("targetUnit"))
+            {
+                this.unit.task.Add(new TaskFight(this.unit.task.removeShared<Unit>("targetUnit")));
+            }
+            return true;
+        }
+
+        public override void onStart()
+        {
+            this.unit.task.Add(new TaskMortality());
+        }
+
+        public override void onFinish()
+        {
+        }
+    }
+}
