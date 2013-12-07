78dbedc01c2dcf2e1b85d890161e3c253df73533
assashin23 <mitchaelsepe@gmail.com>
2013-12-07 00:36:06 -0500

    Added Class to git repo...

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Decal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OppoCraft
+{
+    class Decal : MapEntity
+    {
+        SimpleAnimation animation;
+
+        public Decal(OppoMessage settings)
+        {
+           
+        }
+
+        public override void onStart()
+        {
+            
+        }
+
+        public override void Tick()
+        {
+            
+        }
+
+        public override void Render(RenderSystem render)
+        {
+            
+        }
+    }
+}
