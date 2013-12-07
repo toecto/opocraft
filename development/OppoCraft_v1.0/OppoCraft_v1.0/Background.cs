46d89279c59f4d702051abfb1621620779b47b03
assashin23 <mitchaelsepe@gmail.com>
2013-12-07 00:32:28 -0500

    Added Background.cs to Git Repo..added Variables and onstart method

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Background.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OppoCraft
+{
+    class Background : MapEntity
+    {
+        public int repeatX;
+        public int repeatY;
+        SimpleAnimation animation;
+        public Background(int uid)
+        {
+            this.uid = uid;
+        }
+
+        public override void onStart()
+        {
+            this.size = this.theGame.worldMapSize;
+            this.animation = new SimpleAnimation(this.theGame.graphContent.files["Grass"], 0, 0, 1, 1, false);
+            this.repeatX = this.size.X / this.animation.file.width + 1;
+            this.repeatY = this.size.Y / this.animation.file.height + 1;
+            
+        }
+
+
+}
