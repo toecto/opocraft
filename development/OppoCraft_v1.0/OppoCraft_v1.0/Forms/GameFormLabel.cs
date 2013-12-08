2b92f0dc749380854a9ac24868116b1c26c1134c
tracyljeff <tracyljeff@gmail.com>
2013-12-08 16:57:41 -0500

    GameFormLabel class  inherits from GameFormControl: renders a lable on the screen used on UI form; handles interactions via events in the Forms folder

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Forms/GameFormLabel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OppoCraft
+{
+    class GameFormLabel : GameFormControl
+    {
+        public string Text="Label";
+        public Color color;
+
+        public GameFormLabel(string text)
+        {
+            this.Text = text;
+            this.color = Color.White;
+        }
+
+  
+        public override void Render(RenderSystem render)
+        {
+            Coordinates position = this.ScreenPosition();
+            render.DrawText(this.Text, new Vector2(position.X + 5, position.Y + 3), color);
+            base.Render(render);
+        }
+        
+    }
+}
