3cf106dc4ef9d4e04822b1477e76e2c491d12d98
Freens77 <freens1977@gmail.com>
2013-12-08 00:20:37 -0500

    CommandChangeUnitState: A command passed through the network to change a specific unit's state (can be any of the "stat" properties)
    CommandChangeTreeGrow: A command passed through the network to initiate the "re-growth" of a fallen tree

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Commands/CommandChangeUnitState.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using testClient;
+
+namespace OppoCraft
+{
+    class CommandChangeUnitState: Task
+    {
+        OppoMessage message;
+        public CommandChangeUnitState(OppoMessage message)
+        {
+            this.message = message;
+        }
+
+        public override void onStart()
+        {
+            
+            if (this.message.Text.ContainsKey("stopact"))
+            {
+                this.unit.animation.stopAction(this.message.Text["stopact"]);
+            }
+            if (this.message.Text.ContainsKey("onlyact"))
+            {
+                this.unit.animation.Clear();
+                this.unit.animation.startAction(this.message.Text["onlyact"]);
+            }
+
+            if (this.unit.alive && this.message.Text.ContainsKey("startact"))
+            {
+                this.unit.animation.startAction(this.message.Text["startact"]);
+            }
+
+            if (this.message.Text.ContainsKey("startactforced"))
+            {
+                this.unit.animation.startAction(this.message.Text["startactforced"]);
+            }
+            if (this.message.ContainsKey("addhp"))
+            {
+                this.unit.currHP += this.message["addhp"];
+            } 
+            
+            if (this.message.Text.ContainsKey("status"))
+            {
+                this.unit.status = this.message.Text["status"];
+            }
+            if (this.message.ContainsKey("direction"))
+            {
+                this.unit.direction = (Unit.Direction)this.message["direction"];
+            }
+            
+
+        }
+    }
+}
---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Commands/CommandTreeGrow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using testClient;
+
+namespace OppoCraft
+{
+    class CommandTreeGrow : Task
+    {
+
+        enum Status
+        {
+            Adult,
+            Died,
+            Collected,
+            Grow,
+        }
+
+        Status status;
+
+        int restCooldown;
+
+        public CommandTreeGrow(OppoMessage settings)
+        {
+            this.restCooldown = settings["cooldown"];
+            this.status = Status.Collected;
+        }
+
+        public override bool Tick()
+        {
+            if (this.status == Status.Collected)
+            {
+                this.restCooldown--;
+                if (this.restCooldown < 1)
+                {
+                    this.status = Status.Grow;
+                    this.unit.animation.Clear();
+                    this.unit.animation.startAction("Adult");
+                    this.unit.animation.startAction("Grow");
+                }
+            }
+
+            if (this.status == Status.Grow && this.unit.animation.current.First.Value.name == "Adult")
+            {
+                this.unit.status = "Adult";
+                this.unit.currHP = this.unit.maxHP;
+                return false;
+            }
+            return true;
+        }
+    }
+}
