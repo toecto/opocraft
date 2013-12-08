bbbf485f217cd1bb6fac076c52a6a0ad2b9acec3
tracyljeff <tracyljeff@gmail.com>
2013-12-08 16:10:57 -0500

    UnitCastle class inherits form Unit: holds other settings related to the Castle, such as speed, armor, attack speed, attack range, etc.

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Castle/UnitCastle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using testClient;
+
+namespace OppoCraft
+{
+    public class UnitCastle: Unit
+    {
+        public OppoMessage factorySettings;
+        
+        public UnitCastle(Game1 theGame, OppoMessage settings)
+            : base(theGame,settings)
+        {
+            this.factorySettings = new OppoMessage(OppoMessageType.ChangeState);
+            this.factorySettings.Text["zone"] = "";
+            this.factorySettings["attack"] = 1;
+            this.factorySettings["attackspeed"] = 1;
+            this.factorySettings["attackrange"] = 1;
+            this.factorySettings["viewrange"] = 1;
+            this.factorySettings["speed"] = 1;
+            this.factorySettings["armor"] = 1;  
+            this.factorySettings["viewrange"] = 1;
+            this.factorySettings["buildspeed"] = 1;
+            this.factorySettings.Text["targets"] = "";
+        }
+    }
+}
