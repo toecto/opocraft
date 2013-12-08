17848214114d99e4ac91426e3a273e73ca3bc2d6
assashin23 <mitchaelsepe@gmail.com>
2013-12-08 17:13:18 -0500

    Added GameForm.cs
    Inherits from GameFormControl: renders a form on the screen; displays information related to a selected object;

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Forms/GameForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OppoCraft
+{
+    public class GameForm : GameFormControl
+    {
+        GameFormButton close;
+        public bool onScreen = false;
+        public GameForm()
+        {
+            this.parentForm = this;
+            this.location = new WorldCoords(300, 50);
+            this.size = new WorldCoords(400, 400);
+            this.controls.Add(this.close = new GameFormButton("X"));
+        }
+
+        public override void onStart()
+        {
+            this.onScreen = true;
+
+            this.theGame.unitSelector.enabled = false;
+
+            this.close.location.X = this.size.X - this.close.size.X;
+            this.close.location.Y = 0;
+            this.close.onClick += closeForm;
+        }
+
+        public void closeForm(GameFormControl obj, Coordinates mouse)
+        {
+            this.theGame.forms.Remove(this.uid);
+        }
+
+
+        public override void Tick()
+        {
+            WorldCoords mouse = new WorldCoords(0, 0);
+            mouse.setVector2(theGame.userInput.mousePosition);
+
+            if (this.theGame.userInput.mouseClicked)
+            {
+                this.onClickEvent(mouse);
+            }
+            base.Tick();
+        }
+
+        public override void Render(RenderSystem render)
+        {
+            render.Draw(render.primRect70, this.location.getRectangle(this.size), render.primRect70.Bounds, Color.Black);
+            base.Render(render);
+        }
+
+
+        public override void onFinish()
+        {
+            this.theGame.unitSelector.enabled = true;
+            this.onScreen = false;
+        }
+
+    }
+}
