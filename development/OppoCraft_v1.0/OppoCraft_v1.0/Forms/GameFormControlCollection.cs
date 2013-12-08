9438f4ccb03284f149d4e06b4f7896fa655bda3a
holmesli <holmeslixu@gmail.com>
2013-12-08 16:40:46 -0500

    Inherits from GameFormControl: acts as a container for base controls in the game

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Forms/GameFormControlCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OppoCraft
+{
+    public class GameFormControlCollection: List<GameFormControl>
+    {
+        GameFormControl parent;
+
+
+        public GameFormControlCollection(GameFormControl parent)
+        {
+            this.parent = parent;
+        }
+
+        new public void Add(GameFormControl item)
+        {
+            
+            item.setParent(this.parent);
+            base.Add(item);
+        }
+
+        new public void Remove(GameFormControl item)
+        {
+            base.Remove(item);
+        }
+
+    }
+}
