f6fc6d6537a41a62db42a0a1011d7489d81b0e79
tracyljeff <tracyljeff@gmail.com>
2013-12-08 11:17:35 -0500

    TaskObstacleDriver class inherits from Task: First task added to task collection for an obstacle; much simpler logic vs. other units, as obstacles have very few other tasks that can be added

---
+++ development/OppoCraft_v1.0/OppoCraft_v1.0/Task/TaskObstacleDriver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace OppoCraft
+{
+    class TaskObstacleDriver: Task
+    {
+        public override bool Tick()
+        {
+            return false;
+        }
+
+        public override void onStart()
+        {
+            this.unit.animation.Clear();
+            this.unit.animation.startAction(this.unit.type);
+        }
+
+        public override void onFinish()
+        {
+        }
+    }
+}
